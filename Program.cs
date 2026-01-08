91a65ccfd2129f33a66fbcee6dc5d5bf0131ef7f
tonovarela <tonovarela@live.com>
2026-01-08 16:01:45 -0600

    Optimizar el procesamiento de archivos en Program. Ajustar la lógica para mover y eliminar archivos según su estado. Actualizar el archivo de proyecto para incluir la configuración de .env.

--- Program.cs
+++ Program.cs
@@ -22,33 +22,43 @@ class Program
 
         var archivos = scanner.ObtenerArchivos();
         List<ArchivoPorProcesar> todos = new List<ArchivoPorProcesar>();
+        List<string> entradasMover = new List<string>();
         foreach (string entradaDeCompra in archivos)
         {
             var archivosProcesados = processor.Procesar(entradaDeCompra);
             var idArchivo = archivosProcesados[0].ID;
-            ordenDAO.EliminarAnexoMov(idArchivo.ToString());
+            //ordenDAO.EliminarAnexoMov(idArchivo.ToString());
             List<ArchivoPorProcesar> archivosPorRegistrar = archivosProcesados.Where(a => a.ExisteRutaArchivo()).ToList();
             List<ArchivoPorProcesar> archivosNoEncontrados = archivosProcesados.Where(a => !a.ExisteRutaArchivo()).ToList();            
             todos.AddRange(archivosPorRegistrar);
             todos.AddRange(archivosNoEncontrados);
+            if (archivosNoEncontrados.Count >0)
+            {
+                entradasMover.Add(idArchivo.ToString());
+            }
 
             foreach (ArchivoPorProcesar archivo in archivosPorRegistrar)
             {
                 fileOrder.Copy(archivo.RutaArchivo, archivo.Destino);
                 string destino = archivo.Destino.Replace("Volumes", "192.168.2.217");
-                ordenDAO.registrarArchivoAnexo(destino, archivo.ID, archivo.TipoArchivo.ToString());       
+                //ordenDAO.registrarArchivoAnexo(destino, archivo.ID, archivo.TipoArchivo.ToString());       
             }
                    
         }
         if (todos.Count >0){
-        csvWriter.Write(todos);        
+        csvWriter.Write(todos);
+        List<string> archivosMover = todos
+                                             .Where(x => x.TipoArchivo == TipoArchivo.EC)
+                                             .Where(x => entradasMover.Contains(x.ID.ToString()))
+                                             .Select(x => x.RutaArchivo)
+                                             .ToList(); 
         List<string> archivosEliminar = todos
                                          .Where(x => x.TipoArchivo == TipoArchivo.EC)
+                                         .Where(x => !entradasMover.Contains(x.ID.ToString()))
                                          .Select(x => x.RutaArchivo)
-                                         .ToList();
-                                                 
-        fileOrder.MoveFiles(archivosEliminar,  Path.Combine(conf.HotFolderPath, "procesados"));
-        //fileOrder.EliminarArchivos(archivosEliminar);
+                                         .ToList();                                                                                          
+        fileOrder.MoveFiles(archivosMover,  Path.Combine(conf.HotFolderPath, "no_procesados"));
+        fileOrder.DeleteFiles(archivosEliminar);
         }
         
     }
